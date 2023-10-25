8b27846988ee917c687ce2453fbdba83ee6ddae6
Liz.Wagner <liz.vogner@gmail.com>
2023-10-25 12:08:45 -0600

    switch functions async/await
    code updates from 5.6.7 to switch from .then to async/await

--- src/suppliers/suppliers.controller.js
+++ src/suppliers/suppliers.controller.js
@@ -30,62 +30,53 @@ function hasOnlyValidProperties(req, res, next) {
     next();
 }
 
-function supplierExists(req, res, next) {
-  suppliersService
-    .read(req.params.supplierId)
-    .then((supplier) => {
-      if (supplier) {
-        res.locals.supplier = supplier;
-        return next();
-      }
-      next({
-        status: 404,
-        message: `Supplier cannot be found.`
-      })
-    })
-    .catch(next);
+async function supplierExists(req, res, next) {
+  const supplier = await suppliersService.read(req.params.supplierId)
+  if (supplier) {
+    res.locals.supplier = supplier;
+    return next();
+  }
+  next({
+    status: 404,
+    message: "Supplier cannot be found"
+  });
 }
 
 
-function create(req, res, next) {
-  suppliersService
-    .create(req.body.data)
-    .then((data) => res.status(201).json({ data }))
-    .catch(next);
+async function create(req, res, next) {
+  const data = await suppliersService.create(req.body.data)
+  res.status(201).json({ data });
 }
 
-async function update(req, res, next) {
+async function update(req, res) {
   const updatedSupplier = {
     ...req.body.data,
-    supplierId: res.locals.supplier.supplier_id,
+    supplier_id: res.locals.supplier.supplier_id,
   };
-  suppliersService
-    .update(updatedSupplier)
-    .then((data) => res.json({ data }))
-    .catch(next);
+  const data = await suppliersService.update(updatedSupplier)
+  res.json({ data });
 }
 
-function destroy(req, res, next) {
-  suppliersService
-    .delete(res.locals.supplier.supplier_id)
-    .then(() => res.sendStatus(204))
-    .catch(next);
+async function destroy(req, res) {
+  const { supplier } = res.locals;
+  await suppliersService.delete(supplier.supplier_id)
+  res.sendStatus(204);
 }
 
 module.exports = {
   create: [
-    hasOnlyValidProperties, 
-    hasRequiredProperties, 
-    create
+    hasOnlyValidProperties,
+    hasRequiredProperties,
+    asyncErrorBoundary(create),
   ],
   update: [
-    supplierExists,
-    hasOnlyValidProperties, 
-    hasRequiredProperties, 
-    update
+    asyncErrorBoundary(supplierExists),
+    hasOnlyValidProperties,
+    hasRequiredProperties,
+    asyncErrorBoundary(update),
   ],
   delete: [
-    supplierExists,
-    destroy
+    asyncErrorBoundary(supplierExists), 
+    asyncErrorBoundary(destroy)
   ],
 };
